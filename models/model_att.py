b9b631da80b8f5fee92698287c7da9c282e13960
ha01994 <ha01994@naver.com>
2023-12-15 17:07:12 +0900

    commit

---
+++ models/model_att.py
@@ -0,0 +1,169 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import itertools
+import sys, math
+from .att_utils import *
+import numpy as np
+
+
+
+
+class attention_block(nn.Module):
+    def __init__(self, input_dim, num_heads, dim_feedforward, dropout=0.0):
+        super(attention_block, self).__init__()                
+        self.dropout = dropout
+                
+        self.self_attn1 = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.self_attn2 = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.self_attn3 = MultiheadAttention(input_dim, input_dim, num_heads)
+        
+        self.ffn1 = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),            
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim)
+        )
+        self.ffn2 = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),            
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim)
+        )
+        self.ffn3 = nn.Sequential(
+            nn.Linear(input_dim, dim_feedforward),            
+            nn.Dropout(dropout),
+            nn.ReLU(inplace=True),
+            nn.Linear(dim_feedforward, input_dim)
+        )
+        self.mha1 = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.mha2 = MultiheadAttention(input_dim, input_dim, num_heads)
+        self.mha3 = MultiheadAttention(input_dim, input_dim, num_heads)
+        
+        self.layernorm = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+        
+    def forward(self, pep,a,b):
+        
+        pep_, _ = self.self_attn1(pep,pep,pep)
+        pep = pep + self.dropout(pep_)
+        pep = self.layernorm(pep)
+                
+        a_, _ = self.self_attn2(a,a,a)
+        a = a + self.dropout(a_)
+        a = self.layernorm(a)
+        
+        b_, _ = self.self_attn3(b,b,b)
+        b = b + self.dropout(b_)
+        b = self.layernorm(b)
+        
+        
+        z = torch.cat((a,b),1)
+        pep_, pep_att = self.mha1(pep,z,z)
+        pep = pep + self.dropout(pep_)
+        pep = self.layernorm(pep)
+        
+        a_, a_att = self.mha2(a,pep,pep)
+        a = a + self.dropout(a_)
+        a = self.layernorm(a)
+        
+        b_, b_att = self.mha3(b,pep,pep)
+        b = b + self.dropout(b_)
+        b = self.layernorm(b)   
+        
+        
+        pep_ = self.ffn1(pep)
+        pep = pep + self.dropout(pep_)
+        pep = self.layernorm(pep)
+        
+        a_ = self.ffn2(a)
+        a = a + self.dropout(a_)
+        a = self.layernorm(a)
+                
+        b_ = self.ffn3(b)
+        b = b + self.dropout(b_)
+        b = self.layernorm(b)
+        
+        
+        return pep, a, b, pep_att, a_att, b_att
+    
+        
+    
+    
+
+class attmodel(nn.Module):
+    def __init__(self, max_pep_len, max_tcr_len, max_a1_len, max_a2_len, max_a3_len, max_b1_len, max_b2_len, max_b3_len, dropout):
+        super(attmodel, self).__init__()
+        self.dropout = dropout
+        max_pep_len, max_tcr_len = int(max_pep_len), int(max_tcr_len)
+        max_a1_len, max_a2_len, max_a3_len = int(max_a1_len), int(max_a2_len), int(max_a3_len)
+        max_b1_len, max_b2_len, max_b3_len = int(max_b1_len), int(max_b2_len), int(max_b3_len)
+        max_a_len = max_a1_len+max_a2_len+max_a3_len
+        max_b_len = max_b1_len+max_b2_len+max_b3_len
+                
+        attn_dim = 128
+        self.attn_dim = attn_dim
+        self.emb1 = nn.Embedding(21, attn_dim, padding_idx=0)
+        self.emb2 = nn.Embedding(21, attn_dim, padding_idx=0)
+        self.emb3 = nn.Embedding(21, attn_dim, padding_idx=0)
+                        
+        n_head = 4
+        d_model, d_inner = attn_dim, attn_dim
+        d_k, d_v = int(attn_dim/n_head), int(attn_dim/n_head)
+        
+        self.attention_model = attention_model(num_layers=1, 
+                                              input_dim=attn_dim,
+                                              dim_feedforward = attn_dim,
+                                              num_heads=n_head, 
+                                              dropout=dropout)
+        
+        self.out_mlp = nn.Sequential(
+            nn.Linear(attn_dim*(max_pep_len+max_a_len+max_b_len), attn_dim),
+            nn.BatchNorm1d(attn_dim),
+            nn.ReLU(inplace=True),
+            nn.Linear(attn_dim, 1)
+        )
+        
+            
+    def forward(self, **ft_dict):
+        pep = ft_dict['batch_pep_seq']
+        a = torch.cat([ft_dict['batch_a1_seq'],ft_dict['batch_a2_seq'],ft_dict['batch_a3_seq']], 1)
+        b = torch.cat([ft_dict['batch_b1_seq'],ft_dict['batch_b2_seq'],ft_dict['batch_b3_seq']], 1)
+        
+        pep = self.emb1(pep) #[bsz, seq_len, attn_dim]        
+        a = self.emb2(a)        
+        b = self.emb3(b)
+                
+        pep, a, b, pep_att, a_att, b_att = self.attention_model(pep,a,b)        
+                
+        x = torch.cat([a, b, pep], 1)
+        x = x.flatten(start_dim=1)
+        
+        pred = self.out_mlp(x) #[bsz, attn_dim]
+        pred = torch.sigmoid(pred)
+        pred = pred.view(-1)
+        
+        return pred, pep_att, a_att, b_att
+    
+    
+
+
+
+class attention_model(nn.Module):    
+    def __init__(self, num_layers, **block_args):
+        super(attention_model, self).__init__()
+        self.layers = nn.ModuleList([attention_block(**block_args) for _ in range(num_layers)])
+    
+    def forward(self, pep, a, b):
+        
+        for l in self.layers:
+            pep, a, b, pep_att, a_att, b_att = l(pep, a, b)
+        
+        return pep, a, b, pep_att, a_att, b_att
+    
+    
+    
+
+
+    
---
+++ train.py
@@ -0,0 +1,267 @@
+import os, sys, math, glob, csv
+import numpy as np
+import random
+import pandas as pd
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+from utils.load_dataset import Dataset
+from models.model_cnn import cnnmodel
+from models.model_att import attmodel
+from utils.evaluate import *
+from utils.other import *
+import tqdm
+import shutil
+
+
+
+
+if os.path.isdir('losses/'):
+    shutil.rmtree('losses/')
+if os.path.isdir('save_dir/'):
+    shutil.rmtree('save_dir/')
+
+os.mkdir('losses/')
+os.mkdir('losses/cnn')
+os.mkdir('losses/att')
+os.mkdir('save_dir/')
+os.mkdir('save_dir/cnn')
+os.mkdir('save_dir/att')
+
+
+
+
+def get_loss(pred, label):
+    loss_op = nn.BCELoss()
+    loss = loss_op(pred, label)
+    return loss
+
+
+
+
+
+with open('utils/hyperparams.csv', 'r') as f:
+    r = csv.reader(f)
+    for line in r:
+        if line[0]=='bsz':bsz=int(line[1])
+        if line[0]=='lr_cnn':lr_cnn=float(line[1])
+        if line[0]=='lr_att':lr_att=float(line[1])
+        if line[0]=='epochs_cnn':epochs_cnn=int(line[1])
+        if line[0]=='epochs_att':epochs_att=int(line[1])        
+        if line[0]=='dropout_att':dropout_att=float(line[1])
+        if line[0]=='dropout_cnn':dropout_cnn=float(line[1])
+
+
+
+device = torch.device('cuda:0')
+
+
+
+
+def load_ft_dict(protein_ft_dict, batch_pep_idx, batch_tcr_idx):
+    return {'batch_pep_onehot': protein_ft_dict['pep_onehot'][batch_pep_idx],
+           'batch_a1_onehot': protein_ft_dict['a1_onehot'][batch_tcr_idx],
+           'batch_a2_onehot': protein_ft_dict['a2_onehot'][batch_tcr_idx],
+           'batch_a3_onehot': protein_ft_dict['a3_onehot'][batch_tcr_idx],
+           'batch_b1_onehot': protein_ft_dict['b1_onehot'][batch_tcr_idx],
+           'batch_b2_onehot': protein_ft_dict['b2_onehot'][batch_tcr_idx],
+           'batch_b3_onehot': protein_ft_dict['b3_onehot'][batch_tcr_idx],
+           'batch_pep_seq': protein_ft_dict['pep_seq'][batch_pep_idx],           
+           'batch_a1_seq': protein_ft_dict['a1_seq'][batch_tcr_idx],
+           'batch_a2_seq': protein_ft_dict['a2_seq'][batch_tcr_idx],
+           'batch_a3_seq': protein_ft_dict['a3_seq'][batch_tcr_idx],
+           'batch_b1_seq': protein_ft_dict['b1_seq'][batch_tcr_idx],
+           'batch_b2_seq': protein_ft_dict['b2_seq'][batch_tcr_idx],
+           'batch_b3_seq': protein_ft_dict['b3_seq'][batch_tcr_idx]}
+
+
+
+
+
+def run_evaluation(pair_idx, model, bsz, pairs_pep_indices, pairs_tcr_indices, all_label_mat, protein_ft_dict, model_type):
+    model.eval()
+    preds, labels, losses = [], [], []
+    j = int(len(pair_idx)/bsz) if len(pair_idx)%bsz==0 else int(len(pair_idx)/bsz)+1
+    for i in tqdm.tqdm(range(j)):
+        batch_idx = pair_idx[i*bsz: (i+1)*bsz]
+        batch_pep_idx = pairs_pep_indices[batch_idx]
+        batch_tcr_idx = pairs_tcr_indices[batch_idx]
+        label = torch.FloatTensor(all_label_mat[batch_idx]).to(device)
+        
+        ft_dict = load_ft_dict(protein_ft_dict, batch_pep_idx, batch_tcr_idx)
+
+        if model_type=='cnn':
+            pred = model(**ft_dict)
+        else:
+            pred,_,_,_ = model(**ft_dict)
+        loss = get_loss(pred, label)
+        losses.append(loss.cpu().detach().item())
+        preds.extend(list(pred.cpu().detach().numpy()))
+        labels.extend(list(label.cpu().detach().numpy()))
+    epoch_loss = np.mean(losses)
+    epoch_auroc, epoch_auprc = evaluation_metrics(preds, labels)
+
+    return epoch_loss, epoch_auroc, epoch_auprc, preds, labels
+
+
+           
+    
+    
+
+
+
+save_dir = 'save_dir/cnn/'
+result_dir = 'losses/cnn/'
+
+set_random_seed()
+
+dataset = Dataset('features/')
+protein_ft_dict = dataset.to_tensor(device)
+all_label_mat, pairs_pep_indices, pairs_tcr_indices,\
+max_pep_len, max_tcr_len, train_index, val_index, test_index,\
+max_a1_len, max_a2_len, max_a3_len, max_b1_len, max_b2_len, max_b3_len,\
+= dataset.get_stuff()
+
+print('len(train_index)', len(train_index))
+print('len(val_index)', len(val_index))
+print('len(test_index)', len(test_index))
+
+model = cnnmodel(max_pep_len, max_tcr_len, max_a1_len,max_a2_len,max_a3_len,max_b1_len,max_b2_len,max_b3_len, dropout_cnn).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr_cnn)    
+total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print('Number of parameters: %d'%total_params)
+
+curr_val_auroc = 0
+for epoch in range(1, epochs_cnn+1):            
+    print('-------- epoch %d --------'%epoch)            
+    pair_idx = train_index
+    random.shuffle(pair_idx)
+
+    preds, labels, losses = [], [], []
+    j = int(len(pair_idx)/bsz)
+    for i in tqdm.tqdm(range(j)):
+        model.train()
+        optimizer.zero_grad()            
+        batch_idx = pair_idx[i*bsz: (i+1)*bsz]
+        batch_pep_idx = pairs_pep_indices[batch_idx]
+        batch_tcr_idx = pairs_tcr_indices[batch_idx]
+        label = torch.FloatTensor(all_label_mat[batch_idx]).to(device)
+        ft_dict = load_ft_dict(protein_ft_dict, batch_pep_idx, batch_tcr_idx)            
+        pred = model(**ft_dict)            
+        loss = get_loss(pred, label)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.cpu().detach().item())
+        preds.extend(list(pred.cpu().detach().numpy()))
+        labels.extend(list(label.cpu().detach().numpy()))
+
+    train_loss = np.mean(losses)
+    train_auroc, _ = evaluation_metrics(preds, labels)
+
+    #==================================================================================================================#
+    val_loss, val_auroc, _, _, _ = run_evaluation(val_index, model, bsz, pairs_pep_indices, pairs_tcr_indices, 
+                                                  all_label_mat, protein_ft_dict, 'cnn')
+
+    test_loss, test_auroc, _, preds, labels = run_evaluation(test_index, model, bsz, pairs_pep_indices, pairs_tcr_indices, 
+                                                             all_label_mat, protein_ft_dict, 'cnn')        
+    #==================================================================================================================#
+
+    print('epoch %d'%epoch)
+    print('train loss : %.4f | auroc: %.4f'%(train_loss, train_auroc))
+    print('val loss   : %.4f | auroc: %.4f'%(val_loss, val_auroc))
+    print('test loss  : %.4f | auroc: %.4f'%(test_loss, test_auroc))
+
+    with open(os.path.join(result_dir, 'losses.csv'), 'a') as fw:
+        fw.write('%d,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(epoch, train_loss, train_auroc, val_loss, val_auroc,
+                                                       test_loss, test_auroc))        
+
+    if curr_val_auroc < val_auroc:
+        print('curr_val_auroc < val_auroc')
+        torch.save(model.state_dict(), os.path.join(save_dir, 'model.pt'))
+        curr_val_auroc = val_auroc
+
+
+
+        
+        
+     
+        
+
+save_dir = 'save_dir/att/'
+result_dir = 'losses/att/'
+
+set_random_seed()
+
+dataset = Dataset('features/')
+protein_ft_dict = dataset.to_tensor(device)
+all_label_mat, pairs_pep_indices, pairs_tcr_indices,\
+max_pep_len, max_tcr_len, train_index, val_index, test_index,\
+max_a1_len, max_a2_len, max_a3_len, max_b1_len, max_b2_len, max_b3_len,\
+= dataset.get_stuff()
+
+print('len(train_index)', len(train_index))
+print('len(val_index)', len(val_index))
+print('len(test_index)', len(test_index))
+
+model = attmodel(max_pep_len, max_tcr_len, max_a1_len,max_a2_len,max_a3_len,max_b1_len,max_b2_len,max_b3_len, dropout_att).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=lr_att)    
+total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print('Number of parameters: %d'%total_params)
+
+curr_val_auroc = 0
+for epoch in range(1, epochs_att+1):
+    print('-------- epoch %d --------'%epoch)
+    pair_idx = train_index
+    random.shuffle(pair_idx)
+
+    preds, labels, losses = [], [], []
+    j = int(len(pair_idx)/bsz)
+    for i in tqdm.tqdm(range(j)):
+        model.train()
+        optimizer.zero_grad()            
+        batch_idx = pair_idx[i*bsz: (i+1)*bsz]
+        batch_pep_idx = pairs_pep_indices[batch_idx]
+        batch_tcr_idx = pairs_tcr_indices[batch_idx]
+        label = torch.FloatTensor(all_label_mat[batch_idx]).to(device)
+        ft_dict = load_ft_dict(protein_ft_dict, batch_pep_idx, batch_tcr_idx)
+        pred,_,_,_ = model(**ft_dict)            
+        loss = get_loss(pred, label)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.cpu().detach().item())
+        preds.extend(list(pred.cpu().detach().numpy()))
+        labels.extend(list(label.cpu().detach().numpy()))
+
+    train_loss = np.mean(losses)
+    train_auroc, _ = evaluation_metrics(preds, labels)
+
+    #==================================================================================================================#
+    val_loss, val_auroc, _, _, _ = run_evaluation(val_index, model, bsz, pairs_pep_indices, pairs_tcr_indices, 
+                                                  all_label_mat, protein_ft_dict, 'att')
+
+    test_loss, test_auroc, _, preds, labels = run_evaluation(test_index, model, bsz, pairs_pep_indices, pairs_tcr_indices, 
+                                                             all_label_mat, protein_ft_dict, 'att')        
+    #==================================================================================================================#
+
+    print('epoch %d'%epoch)
+    print('train loss : %.4f | auroc: %.4f'%(train_loss, train_auroc))
+    print('val loss   : %.4f | auroc: %.4f'%(val_loss, val_auroc))
+    print('test loss  : %.4f | auroc: %.4f'%(test_loss, test_auroc))
+
+    with open(os.path.join(result_dir, 'losses.csv'), 'a') as fw:
+        fw.write('%d,%.4f,%.4f,%.4f,%.4f,%.4f,%.4f\n'%(epoch, train_loss, train_auroc, val_loss, val_auroc,
+                                                       test_loss, test_auroc))        
+
+    if curr_val_auroc < val_auroc:
+        print('curr_val_auroc < val_auroc')
+        torch.save(model.state_dict(), os.path.join(save_dir, 'model.pt'))
+        curr_val_auroc = val_auroc
+
+
+
+        
+        
+        
+        
+        
+        
